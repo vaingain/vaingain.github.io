a9ae48332a8af08ccb9657399f3a8d1706fe76e8
vaingain <64058833+vaingain@users.noreply.github.com>
2020-07-08 17:08:54 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,20 @@
+document.querySelector('img') = function{
+   alert('hello');
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
